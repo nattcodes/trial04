ebf034114427ef3d3aac41a5706a7b44b6f481b1
Kome Nathaniel <77756722+nattcodes@users.noreply.github.com>
2022-10-19 20:20:35 +0100

    start

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import Header from "./Header";
+import Nav from "./Nav";
+import Footer from "./Footer";
+import Home from "./Home";
+import NewPost from "./NewPost";
+import PostPage from "./PostPage";
+import About from "./About";
+import Missing from "./Missing";
+import {Route, Switch, useHistory} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
+      <Nav/>
+      <Home/>
+      <NewPost/>
+      <PostPage/>
+      <About/>
+      <Missing/>
+      <Footer/>
     </div>
   );
 }
